980a43bd78496248c80392c269392a60e4cc2398
Menachem A. Salomon <msalomon@bhphoto.com>
2015-04-02 18:43:49 -0400

    Copy jcal.java from my local repository
    This file is the main command line interface, which is why, contrary to convention, it is a class file with a lowercase name.

---
+++ src/jcal.java
@@ -0,0 +1,39 @@
+/** This class contains the command line interface to the Hebrew calendar
+ * program.  It is named in lowercase (contrary to convention) for ease of use.
+ * @author Menachem A. Salomon
+ * @see Molad
+ * @see Hyear
+ * @see Hmonth
+ * @see CalendarFrame
+ */
+public class jcal {
+
+	/** Run from the command line.
+	 * Synopsis: java jcal [options] <year> <month>
+	 * Options: [ none yet ] */
+	public static void main(String[] args) {
+		// TO DO: Set options here.
+		if (args.length != 2) {
+			System.err.println(usage_str);
+			System.exit(1);
+		}
+
+		// acquire year value:
+		int yr = Integer.parseInt(args[0]);
+		Hyear year = new Hyear(yr);
+
+		// acquire month value
+		// Future Versions: read month as word, get leap months right
+		int mo = Integer.parseInt(args[1]);
+		Hmonth month = new Hmonth(year, mo);
+
+		// Do output:
+		System.out.println("MS's Jewish Calendar, " + version_str);
+		System.out.printf("Calendar for %s, %d%n", month.getName(), year.getYear());
+		month.getMolad().display();
+		month.display(true);
+	}
+
+	private static String version_str = "version 0.2";
+	private static String usage_str = "Usage: java jcal year month";
+}
